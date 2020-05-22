60a508ea6d22ad264d80916f23477eb197255b22
Srijit RK <srijit@outlook.com>
2020-05-22 12:04:50 +0530

    First level modifications

---
+++ .out/precache-manifest.8285846a8f723a06b4e1967d918d9294.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1a58874f8016f62f0f86cb5645bffd",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a72118172a276af2cc65.bundle.js"
+  },
+  {
+    "url": "runtime~main.a72118172a276af2cc65.bundle.js"
+  },
+  {
+    "url": "vendors~main.a72118172a276af2cc65.bundle.js"
+  },
+  {
+    "url": "vendors~main.a72118172a276af2cc65.bundle.js.LICENSE.txt"
+  }
+]);
